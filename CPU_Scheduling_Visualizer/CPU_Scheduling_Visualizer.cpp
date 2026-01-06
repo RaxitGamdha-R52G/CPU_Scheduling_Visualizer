ac80e89d02c8813022e447a8cd7a720605a3e251
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2026-01-06 14:36:52 +0530

    feat(core): add non-preemptive SJF scheduling algorithm

--- CPU_Scheduling_Visualizer/CPU_Scheduling_Visualizer.cpp
+++ CPU_Scheduling_Visualizer/CPU_Scheduling_Visualizer.cpp
@@ -2,21 +2,39 @@
 #include "api/SchedulerAPI.h"
 #include "api/ProcessAPI.h"
 #include "core/SchedulerEngine/FCFS/FCFSScheduler.h"
+#include "core/SchedulerEngine/SJF/SJFScheduler.h"
 #include "visualization/TableFormatter/TableFormatter.h"
 #include "visualization/GanttChartASCII/GanttChartASCII.h"
 
 int main()
 {
+	// FCFS
 	SchedulerAPI::initialize();
 
-	ProcessAPI::addProcess(8, 4);
-	ProcessAPI::addProcess(2, 3);
-	ProcessAPI::addProcess(6, 3);
+	ProcessAPI::addProcess(0, 8);
+	ProcessAPI::addProcess(1, 4);
+	ProcessAPI::addProcess(2, 9);
+	ProcessAPI::addProcess(3, 5);
+	ProcessAPI::addProcess(6, 2);
 
 	FCFSScheduler::run();
 
 	TableFormatter::printProcessTable();
-
 	GanttChartASCII::print();
+
+	// SJF non-preemptive
+	SchedulerAPI::initialize();
+
+	ProcessAPI::addProcess(0, 8);
+	ProcessAPI::addProcess(1, 4);
+	ProcessAPI::addProcess(2, 9);
+	ProcessAPI::addProcess(3, 5);
+	ProcessAPI::addProcess(6, 2);
+
+	SJFScheduler::run();
+
+	TableFormatter::printProcessTable();
+	GanttChartASCII::print();
+
 	return 0;
 }
---
+++ CPU_Scheduling_Visualizer/core/SchedulerEngine/SJF/SJFScheduler.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class SJFScheduler {
+public:
+	static void run();
+};
